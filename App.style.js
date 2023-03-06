56a55dc22d953d5054919a1a934405f5c1780152
Feyyaz Numan Cavlak <feyyum@Feyyaz-MacBook-Air.local>
2023-03-06 13:02:12 +0300

    Add React Navigaton and Stack navigation

---
+++ App.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+import { Palette } from './constants/Colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Palette.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
